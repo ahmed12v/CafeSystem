c17a2527eb88d41224d02db17184bf938b9909a4
ahmed12v <ahnurmed16@gmail.com>
2025-01-02 23:32:18 +0200

    UPDATE ON MANGE-EMP

--- src/app/pages/mang-emp/mang-emp.component.ts
+++ src/app/pages/mang-emp/mang-emp.component.ts
@@ -6,56 +6,48 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-mang-emp',
   standalone: true,
- // imports: [RouterLink],
+  imports: [RouterLink],
   templateUrl: './mang-emp.component.html',
-  styleUrl: './mang-emp.component.css'
+  styleUrl: './mang-emp.component.css',
 })
 export class MangEmpComponent implements OnInit {
   ngOnInit(): void {
-    if(typeof localStorage != 'undefined')
-      localStorage.setItem('last Page' , '/employee')
-////////////////////////////call//////////////////////////
-   this.getEmps();
-   
+    if (typeof localStorage != 'undefined')
+      localStorage.setItem('last Page', '/employee');
+    ////////////////////////////call//////////////////////////
+    this.getEmps();
   }
   ////////////////////////////constractour///////////////////////
-constructor(private _EmployeService:EmployeService){}
+  constructor(private _EmployeService: EmployeService) {}
   ////////////////////// get emp////////////////////////////////
- getEmps()
- {
-  this.dataCome=true;
+  getEmps() {
+    this.dataCome = true;
     this._EmployeService.getAllEmp().subscribe({
-      next:res=>{
-        this.dataCome=false;
-        this.empList=res.data;
-      // console.log(this.empList);
-       
+      next: (res) => {
+        this.dataCome = false;
+        this.empList = res.data;
+        // console.log(this.empList);
       },
-      error:err=>{
+      error: (err) => {
         console.log(err);
-        this.dataCome=false;
+        this.dataCome = false;
+      },
+    });
+  }
+  //////////////////////////////////////////////////////////////////////////////////
+  empList!: EmpData[];
+  dataCome: boolean = false;
+  //////////////////////////////////////////////////////////////////////////////////
 
-        
-      }
-    })
- }
- //////////////////////////////////////////////////////////////////////////////////
- empList!:EmpData[];
- dataCome:boolean =false;
- //////////////////////////////////////////////////////////////////////////////////
-  
- deletemp(empid:string){
-  this._EmployeService.deletupdatEmp(empid).subscribe({
-    next:res=>{
-      this.getEmps()
-       //    console.log(res);
-           
-           
-    },
-    error:err=>{
-      console.log(err);
-      
-    }
-  })
- }
+  deletemp(empid: string) {
+    this._EmployeService.deletupdatEmp(empid).subscribe({
+      next: (res) => {
+        this.getEmps();
+        //    console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
